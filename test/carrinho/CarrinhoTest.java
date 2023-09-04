c707a44f966fe22582e91d8d9a4e2a69804fc8f4
Brendo Costa <brendocosta@id.uff.br>
2023-09-03 21:19:15 -0300

    Adiciona teste para esvaziamento do carrinho

--- test/carrinho/CarrinhoTest.java
+++ test/carrinho/CarrinhoTest.java
@@ -57,5 +57,23 @@ public class CarrinhoTest {
         Assertions.assertEquals(0, this.car.getQtdeItems());
 
 	}
+
+    @DisplayName("Testa o esvaziamento do carrinho")
+	@Test
+	public void testEsvaziarCarrinho() {
+
+        final int QUANTIDADE_ITEMS = 10;
+
+        for (int i = 0; i < QUANTIDADE_ITEMS; i++) {
+
+            Produto prod = new Produto("Produto Teste " + i, 10);
+            this.car.addItem(prod);
+
+        }
+
+        this.car.esvazia();
+        Assertions.assertEquals(0, this.car.getQtdeItems());
+
+    }
     
 }
